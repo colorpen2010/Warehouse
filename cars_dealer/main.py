3dbb08ac592453a60d57500ee08342aa09ba6fd3
GmodHunter <ggmodhunters@gmail.com>
2026-04-24 13:13:39 +0300

    version 1

--- cars_dealer/main.py
+++ cars_dealer/main.py
@@ -1,33 +1,47 @@
 import sqlite3
 from rich.console import Console
 from rich.table import Table
-mark_inpt=None
-connection = sqlite3.connect("C:/base/DB")
+
+mark_inpt = None
+connection = sqlite3.connect("..\\DB")
 console = Console()
-
-
-
+strk = f"""
+        SELECT marka,price,probeg,date 
+        FROM car
+        """
 
 while True:
     print("0 : открыть каталог")
     print("1 : отфильтровать по марке")
     print("5 : выйти")
-    inpt = int(input())
+    inpt = int(input("введите вариант: "))
     if inpt == 0:
         table = Table(title="Каталог")
         table.add_column("Марка")
         table.add_column("Цена")
         table.add_column("Пробег")
         table.add_column("Дата")
-        response = connection.execute(f"""
-        SELECT marka,price,probeg,date 
-        FROM car
-        WHERE car.marka = '{mark_inpt}'""")
+        if mark_inpt == None:
+            response = connection.execute(strk)
+        else:
+            strk_addon = f"""WHERE car.marka IN '{mark_inpt}'"""
+            response = connection.execute(strk + strk_addon)
         for i in response:
-                table.add_row(i[0], str(i[1]), str(i[2]), str(i[3]))
+            table.add_row(i[0], str(i[1]), str(i[2]), str(i[3]))
         console.print(table)
     if inpt == 5:
         break
     if inpt == 1:
-        mark_inpt=input("введите марку: ")
-
+        print("0 : убрать фильтр")
+        print("1 : выбрать марку")
+        insrt = int(input("введите вариант: "))
+        if insrt == 1:
+            mark_inpt = input("введите марку: ")
+        elif insrt == 0:
+            mark_inpt = None
+            if ' ' in mark_inpt:
+                ','.
+        else:
+            print("введено неверное число")
+    else:
+        print("введено неверное число")
